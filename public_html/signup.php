4341370d674f746890b144730f430bdcf1014928
motomotoyoshi <motomoto.git@gmail.com>
2019-07-14 14:28:46 +0000

    update, signup validation

--- public_html/signup.php
+++ public_html/signup.php
@@ -2,31 +2,29 @@
 
   require_once(__DIR__ . '/../config/config.php');
 
-  // $app = new MyApp\Controller\index();
+  $app = new MyApp\Controller\Signup();
 
-  // $app->run();
+  $app->run();
 
-  // echo "login screen";
-  // exit;
   ?>
   <!DOCTYPE html>
   <html lang="ja">
   <head>
     <meta charset="UTF-8">
-    <title>Login</title>
+    <title>Signup</title>
     <link rel="stylesheet" href="styles.css">
   </head>
   <body>
    <div id="container">
-    <form action="" method="post">
+    <form action="" method="post" id="signup">
       <p>
         <input type="text" name="email" placeholder="email">
       </p>
       <p>
         <input type="password" name="password" placeholder="password">
       </p>
-      <div class="btn">Login</div>
-      <p class="fs12"><a href="/signup.php">Signup</a></p>
+      <div class="btn" onclick="document.getElementById('signup').submit();">Signup</div>
+      <p class="fs12"><a href="/login.php">Login</a></p>
     </form>
    </div> 
   </body>
