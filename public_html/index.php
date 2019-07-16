2d6c88f7a7d17e5985607e333cb2e2aea1ecc106
motomotoyoshi <motomoto.git@gmail.com>
2019-07-16 16:03:58 +0000

    update, index  styles

--- public_html/index.php
+++ public_html/index.php
@@ -6,4 +6,28 @@
 
   // $app->run();
 
-  var_dump($_SESSION['me']);
+  // var_dump($_SESSION['me']);
+  ?>
+
+  <!DOCTYPE html>
+  <html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+   <div id="container">
+    <form action="logout.php" method="post" id="logout">
+      hoge@example.com <input type="submit" value="Logout" />
+      <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
+    </form>
+    <h1>Users <span class="fs12">(3)</span></h1>
+    <ul>
+      <li>hoge@example.com</li>
+      <li>hoge@example.com</li>
+      <li>hoge@example.com</li>
+    </ul>
+   </div> 
+  </body>
+  </html>
